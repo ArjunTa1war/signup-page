5d5d46a01642bae58063c7aad9d0706db14bd0f7
Arjun Talwar <arjuntalwar1110@gmail.com>
2022-11-16 17:33:28 +0530

    4th commit

--- app.js
+++ app.js
@@ -57,6 +57,6 @@ app.post("/failure",function(req,res){
      res.redirect("/");
 })
 
-app.listen(process.env.Port||3000,function(){
+app.listen(process.env.PORT ||3000,function(){
     console.log("server is running on port 3000");
 }) 
